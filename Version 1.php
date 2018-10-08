40291ae631f2cdb5916d5fd4a7e7525552b93b50
fatin <35326477+fatinadlin@users.noreply.github.com>
2018-10-08 14:47:21 +0800

    Add files via upload

---
+++ Version 1.php
@@ -0,0 +1,24 @@
+<?php
+
+//Array Jujukan
+
+$result = array ( //array = pertama pelajar
+		"Riyad" => array ('KPD4015'=>80 'SEJARAH'=>80. 'DISCRETE MATH'=>90, 'kpd4024'=>85, 'P.ISLAM=>90),
+		"Hamsyari" => array ('KPD4015'=>70, 'SEJARAH'=>76. 'DISCRETE MATH'=>92, 'kpd4024'=>87, 'P.ISLAM'=>91),
+		"Fatin Adlin" => array ('KPD4015'=>90, 'SEJARAH'=>92. 'DISCRETE MATH'=>73, 'kpd4024'=>89, 'P.ISLAM'=>78),
+		"Faheem Aideed" => array ('KPD4015'=>60, 'SEJARAH'=>72. 'DISCRETEMATH'=>83, 'kpd4024'=>79, 'P.ISLAM'=>75),
+		"Nadia Nabila" => array ('KPD4015'=>96, 'SEJARAH'=>92. 'DISCRETE MATH'=>93, 'kpd4024'=>82, 'P.ISLAM'=>76)
+		);
+foreach ($result as $Pelajar => $value(
+	{
+		echo "Pelajar:" .$pelajar;
+		echo "<br>";
+	} foreach ($value as $subjek => rst)
+	{
+		eco "Subjek:" .$subjek."Result:".$rst;
+		echo "<br>";
+
+	}	echo "------------------------------------------------------";
+		echo "<br>";
+	
+?
